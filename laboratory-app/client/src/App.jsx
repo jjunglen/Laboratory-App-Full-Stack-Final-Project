e9bfc227757f4b28d03e7deccec0a320c0b204e4
John Paul Junglen <j.junglen@gmail.com>
2026-05-05 08:34:17 -0500

    phase 1 complete --- set up the basic structure of the app with a simple express backend to make sure both are running

---
+++ laboratory-app/client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css"
+
+function App() {
+  
+
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default App
---
+++ laboratory-app/server/server.js
@@ -0,0 +1,31 @@
+// Load environment variable first
+require("dotenv").config();
+// Required
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(helmet());
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+    res.json({
+        status: "OK",
+        message: "Laboratory server is running",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Laboratory server running on http://localhost:${PORT}`);
+})
